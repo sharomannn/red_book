01f69642d9a2a046c063b3d234f468223f6f9487
roman <r.sharipov@itcodegroup.ru>
2024-09-18 22:06:07 +0500

    #11 Работает парсинг ссылок

--- backend/core/management/commands/parse_redbook.py
+++ backend/core/management/commands/parse_redbook.py
@@ -1,13 +1,12 @@
 import os
 import requests
+from requests.exceptions import RequestException, SSLError
 from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
-from django.core.files import File
-from django.core.files.temp import NamedTemporaryFile
 from django.conf import settings  # Нужно для MEDIA_ROOT
 from core.models import RedBookEntry, Order, Family, Section
 from core.consts import CATEGORY
-
+import re
 
 # Словарь для сопоставления категорий по первым двум словам
 category_mapping = {
@@ -18,104 +17,185 @@ category_mapping = {
     '5-я категория.': CATEGORY.RECOVERING_SPECIES,
 }
 
-# Базовый URL для изображений
+# Базовый URL для скачивания страниц
 BASE_URL = "https://cicon.ru"
 
 class Command(BaseCommand):
-    help = 'Парсинг HTML файлов для Красной книги'
+    help = 'Скачивание и парсинг HTML файлов для Красной книги'
 
     def add_arguments(self, parser):
-        parser.add_argument('file_path', type=str, help='Путь к HTML файлу')
+        parser.add_argument('directory_path', type=str, help='Путь к директории для сохранения HTML файлов')
 
     def handle(self, *args, **kwargs):
-        file_path = kwargs['file_path']
-        if not os.path.exists(file_path):
-            self.stdout.write(self.style.ERROR(f'Файл {file_path} не найден'))
-            return
+        directory_path = kwargs['directory_path']
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
 
-        # Открываем и парсим файл
-        with open(file_path, 'r', encoding='utf-8') as file:
-            soup = BeautifulSoup(file, 'html.parser')
+        # Задаём список ссылок для скачивания
+        links = [
+            "/spisok-pt-mosobl.html", "/malaya-poganka-mosobl.html", "/podicepsgrisegena-mo.html",
+            "/upper-ford-near.html", "/cap_bily.html", "/ciconia_nigra_mosobl.html", "/anser_anser_mo.html",
+            "/anser_erythropus_mo.html", "/cygnus_cygnus_mo.html", "/anas_strepera_mo.html",
+            "/northern_pintails.html", "/skopa-mo.html", "/osoed-mo.html", "/milvus_migrans_govinda.html",
+            "/northern_hen_harrier.html", "/pallid_harrier_mo.html", "/harrier_female.html", "/zmeeyad-mo.html",
+            "/booted_eagle_mo.html", "/greater_Sspotted_eagle.html", "/aquila_pomarina_orlik.html",
+            "/aquila_chrysaetos.html", "/haliaeetus_albicilla.html", "/falco_cherrug_mo.html",
+            "/falco_peregrinus_mo.html", "/falco_columbarius_male.html", "/falco_vespertinus_mo.html",
+            "/willow_grouse_standing.html", "/grus_grus_mo.html", "/pastushok-mo.html", "/porzana_parva.html",
+            "/haematopus_ostralegus.html", "/common_greenshank.html", "/travnik-mo.html", "/tringa_stagnatilis.html",
+            "/xenus_cinereus.html", "/philomachus_pugnax.html", "/gallinago_media.html", "/kronshep-mo.html",
+            "/limosa_limosa.html", "/malaya-chaika.html", "/chlidoniasLeucopterus.html", "/sterna_albifrons.html",
+            "/columbaoenas.html", "/bubo_bubo_mo.html", "/centro_provinciale.html", "/domovoi-sich-mo.html",
+            "/surnia-ulula.html", "/strix_uralensis_mo.html", "/strix_nebulosa.html", "/coracias-garrulus-mo.html",
+            "/alcedo-atthis-mo.html", "/common_hoopoe.html", "/picus-viridis-mo.html", "/sedoi-dyatel-mo.html",
+            "/dendrocopos_medius.html", "/dendrocopos_leucotos_mo.html", "/picoides-tridactylus-mo.html",
+            "/ullula-arborea-mo.html", "/lanius-excubitor-mo.html", "/nucifraga-caryocatactes-mo.html",
+            "/seggenrohrsaenger_hand.html", "/sylvia_nisoria.html", "/remiz_pendulinus.html", "/parus-cyanus-mo.html",
+            "/sadovay-ovsyanka.html", "/emberiza-aureola.html"
+        ]
 
-        # Извлекаем название и латинское название
-        name = soup.find('h1').get_text(strip=True)
-        latin_name = soup.find('pre').get_text(strip=True).split('\n')[1].strip()
+        # Скачиваем и сохраняем HTML файлы
+        for link in links:
+            self.download_html(link, directory_path)
 
-        # Извлечение категории
-        status_paragraph = None
-        for paragraph in soup.find_all('p'):
-            if 'Cтатус' in paragraph.get_text():
-                status_paragraph = paragraph
-                break
+        # После скачивания файлов, начинаем парсинг
+        for filename in os.listdir(directory_path):
+            file_path = os.path.join(directory_path, filename)
+            if os.path.isfile(file_path) and file_path.endswith('.html'):
+                # Парсинг и сохранение каждого HTML файла
+                self.parse_and_save_html(file_path)
 
-        if status_paragraph:
-            status_text = status_paragraph.get_text(strip=True).replace('Cтатус.', '').strip()
-            category_key = ' '.join(status_text.split()[:2])  # Первые два слова
-            category = category_mapping.get(category_key, CATEGORY.UNKNOWN_SPECIES)
-        else:
-            category = CATEGORY.UNKNOWN_SPECIES
+    def download_html(self, link, save_dir):
+        """Функция для скачивания HTML файла с тайм-аутом и обработкой ошибок."""
+        url = BASE_URL + link
+        try:
+            response = requests.get(url, timeout=10)  # Тайм-аут 10 секунд
+            response.raise_for_status()  # Возбудит исключение, если статус код не 200
+            filename = os.path.join(save_dir, os.path.basename(link) + ".html")
+            with open(filename, 'w', encoding='utf-8') as file:
+                file.write(response.text)
+            self.stdout.write(self.style.SUCCESS(f'Скачан файл: {filename}'))
+        except (RequestException, SSLError) as e:
+            self.stdout.write(self.style.ERROR(f'Ошибка при скачивании {url}: {e}'))
 
-        # Извлекаем названия отряда и семейства
-        order_name = soup.find('pre').get_text(strip=True).split('\n')[3].split(' - ')[0].strip()
-        family_name = soup.find('pre').get_text(strip=True).split('\n')[4].split(' - ')[0].strip()
+    import re
 
-        # Получаем или создаем объекты Order и Family
-        order_obj, _ = Order.objects.get_or_create(name=order_name)
-        family_obj, _ = Family.objects.get_or_create(name=family_name, order=order_obj)
+    def parse_and_save_html(self, file_path):
+        """Парсит HTML файл и обновляет или сохраняет данные в базу."""
+        try:
+            # Открываем и парсим файл
+            with open(file_path, 'r', encoding='utf-8') as file:
+                soup = BeautifulSoup(file, 'html.parser')
 
-        # Извлечение названия секции
-        section_nav = soup.find('nav').find_all('a')[-1].get_text(strip=True)
-        section_obj, _ = Section.objects.get_or_create(name=section_nav)
+            # Извлекаем название и латинское название
+            name_tag = soup.find('h1')
+            if name_tag:
+                name = name_tag.get_text(strip=True)
+            else:
+                self.stdout.write(self.style.WARNING(f'Название не найдено в файле {file_path}'))
+                name = "Неизвестное название"
 
-        # Извлечение полезного HTML описания
-        distribution_section = soup.find('h3', text='Распространение.')
-        sources_section = soup.find('h4', text='Источники информации')
+            pre_tag = soup.find('pre')
+            if pre_tag:
+                pre_text_lines = pre_tag.get_text(strip=True).split('\n')
+                latin_name = pre_text_lines[1].strip() if len(pre_text_lines) > 1 else "Неизвестное латинское название"
+            else:
+                self.stdout.write(self.style.WARNING(f'Латинское название не найдено в файле {file_path}'))
+                latin_name = "Неизвестное латинское название"
 
-        # Начинаем с включения самого тега <h3> "Распространение."
-        description_html = str(distribution_section)
+            # Проверка категории статуса
+            status_paragraph = None
+            for paragraph in soup.find_all('p'):
+                if 'Cтатус' in paragraph.get_text():
+                    status_paragraph = paragraph
+                    break
 
-        # Извлечение текста между заголовками "Распространение" и "Источники информации"
-        for tag in distribution_section.find_all_next():
-            if tag == sources_section:
-                break
-            description_html += str(tag)
+            if status_paragraph:
+                status_text = status_paragraph.get_text(strip=True).replace('Cтатус.', '').strip()
+                category_key = ' '.join(status_text.split()[:2])  # Первые два слова
+                category = category_mapping.get(category_key, CATEGORY.UNKNOWN_SPECIES)
+            else:
+                self.stdout.write(self.style.WARNING(f'Категория не найдена в файле {file_path}'))
+                category = CATEGORY.UNKNOWN_SPECIES
 
-        # Сохранение фото на сервер
-        image_tag = soup.find('img', class_='image-art')
-        image_path = None
-        if image_tag:
-            image_url = image_tag['src']
-            if image_url.startswith('/'):  # Проверяем, если путь относительный
-                image_url = BASE_URL + image_url  # Добавляем базовый URL
-            image_name = os.path.basename(image_url)
+            # Проверка на отряд и семейство
+            if pre_tag and len(pre_text_lines) >= 5:
+                order_name = pre_text_lines[3].split(' - ')[0].strip()
+                family_name = pre_text_lines[4].split(' - ')[0].strip()
+            else:
+                order_name = "Неизвестный отряд"
+                family_name = "Неизвестное семейство"
 
-            # Директория для изображений (используем settings.MEDIA_ROOT)
-            image_dir = os.path.join(settings.MEDIA_ROOT, 'red_book_images')
-            os.makedirs(image_dir, exist_ok=True)
+            order_obj, _ = Order.objects.get_or_create(name=order_name)
+            family_obj, _ = Family.objects.get_or_create(name=family_name, order=order_obj)
 
-            # Скачиваем и сохраняем изображение
-            image_temp = NamedTemporaryFile(delete=True)
-            img_response = requests.get(image_url)
-            if img_response.status_code == 200:
-                image_temp.write(img_response.content)
-                image_temp.flush()
+            # Проверяем наличие навигации для секции
+            section_nav = soup.find('nav')
+            if section_nav:
+                section_links = section_nav.find_all('a')
+                section_name = section_links[-1].get_text(strip=True) if section_links else "Неизвестная секция"
+            else:
+                section_name = "Неизвестная секция"
+            section_obj, _ = Section.objects.get_or_create(name=section_name)
 
-                # Путь к файлу изображения
-                image_path = os.path.join(image_dir, image_name)
-                with open(image_path, 'wb') as out_file:
-                    out_file.write(img_response.content)
+            # Ищем секцию "Распространение" с частичным совпадением
+            distribution_section = soup.find('h3', text=re.compile('Распространение', re.IGNORECASE))
+            description_html = ""
+            if distribution_section:
+                description_html += str(distribution_section)
+            else:
+                self.stdout.write(self.style.WARNING(f'Секция "Распространение" не найдена в файле {file_path}'))
 
-        # Сохранение данных в базу
-        entry = RedBookEntry(
-            name=name,
-            latin_name=latin_name,
-            category=category,
-            order=order_obj,
-            family=family_obj,
-            section=section_obj,  # Связываем запись с секцией
-            description=description_html,  # Сохраняем только извлеченный контент с тегом <h3>
-            image=os.path.join('red_book_images', image_name) if image_path else None  # Относительный путь к изображению
-        )
-        entry.save()
+            # Ищем секцию "Источники информации" с частичным совпадением
+            sources_section = soup.find('h4', text=re.compile('Источники информации', re.IGNORECASE))
+            recommendation_section = soup.find('h4',
+                                               text=re.compile('Рекомендации по разведению в неволе', re.IGNORECASE))
 
-        self.stdout.write(self.style.SUCCESS(f'Запись для {name} успешно сохранена.'))
+            # Определяем секцию завершения описания (либо Источники, либо Рекомендации)
+            end_section = sources_section or recommendation_section
+
+            if end_section:
+                # Собираем текст до конца раздела с описанием
+                for tag in distribution_section.find_all_next():
+                    if tag == end_section:
+                        break
+                    description_html += str(tag)
+                description_html += str(end_section)
+            else:
+                # Если не найдено ни одной из секций, собираем до конца значимых секций, но не включаем рекламу и прочие блоки
+                stop_tags = ['aside', 'footer', 'script', 'div', 'form',
+                             'nav']  # Теги, которые сигнализируют конец значимого контента
+                for tag in distribution_section.find_all_next():
+                    if tag.name in stop_tags:
+                        break
+                    description_html += str(tag)
+
+            # Проверка на изображение
+            image_tag = soup.find('img', class_='image-art')
+            image_path = None
+            if image_tag:
+                image_url = image_tag['src']
+                image_name = os.path.basename(image_url)
+                image_path = os.path.join('red_book_images', image_name)
+
+            # Сохранение данных в базу: обновление или создание записи
+            entry, created = RedBookEntry.objects.update_or_create(
+                name=name,  # Используем уникальное название как критерий поиска
+                defaults={
+                    'latin_name': latin_name,
+                    'category': category,
+                    'order': order_obj,
+                    'family': family_obj,
+                    'section': section_obj,
+                    'description': description_html if description_html else "Описание не найдено",
+                    'image': image_path if image_path else None
+                }
+            )
+
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Создана новая запись для {name}.'))
+            else:
+                self.stdout.write(self.style.SUCCESS(f'Обновлена запись для {name}.'))
+
+        except Exception as e:
+            self.stdout.write(self.style.WARNING(f'Ошибка обработки файла {file_path}: {e}'))
