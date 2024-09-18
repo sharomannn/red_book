ecb3ad71f1fb000363c164383e9052eea0b4c825
andreyBelyaev111 <uvnrejnvekjrnvuvjehvjoevjow@gmail.com>
2024-09-19 01:18:06 +0300

    #6 багофиксы микроверстка маршруты конфиг либы мапа

---
+++ frontend/middleware/auth.ts
@@ -0,0 +1,6 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  // isAuthenticated() - это пример метода, проверяющего, аутентифицирован ли пользователь.
+  //   if (isAuthenticated() === false) {
+  //     return navigateTo("/auth-form");
+  //   }
+});
