b5bce74fd24412f12ba4896b5ca3cdbe3656309f
andreyBelyaev111 <uvnrejnvekjrnvuvjehvjoevjow@gmail.com>
2024-09-18 18:45:03 +0300

    #6 test карты

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { createResolver } from "@nuxt/kit";
+const { resolve } = createResolver(import.meta.url);
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
@@ -8,7 +10,29 @@ export default defineNuxtConfig({
     layoutTransition: { name: "layout", mode: "out-in" },
   },
   modules: [
-    '@nuxtjs/eslint-module',
-    '@pinia/nuxt',
-  ]
+    "@nuxtjs/eslint-module",
+    "@pinia/nuxt",
+    "@vueuse/nuxt",
+    "@nuxt/ui",
+    "vue-yandex-maps/nuxt",
+  ],
+  yandexMaps: {
+    apikey: "409c07d0-9651-4cc8-8344-83e160f0263d",
+  },
+  components: [
+    {
+      prefix: "Layout",
+      path: resolve("./components/layouts"),
+      global: true,
+    },
+    {
+      prefix: "Awesome",
+      path: resolve("./components/awesome"),
+      global: true,
+    },
+  ],
+
+  imports: {
+    dirs: [resolve("./stores"), "~/stores"],
+  },
 });
