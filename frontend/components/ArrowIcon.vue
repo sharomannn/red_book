ba2fb9d741e3481196ad74f2fb5b12bc7748e9fb
nikita-nafikov <nafikov.nik@mail.ru>
2024-09-19 11:14:00 +0500

    #5 верстка

---
+++ frontend/components/ArrowIcon.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg
+    width="17"
+    height="17"
+    viewBox="0 0 17 17"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8.5 14.5C11.8137 14.5 14.5 11.8137 14.5 8.5C14.5 5.18629 11.8137 2.5 8.5 2.5C5.18629 2.5 2.5 5.18629 2.5 8.5C2.5 11.8137 5.18629 14.5 8.5 14.5Z"
+      stroke="white"
+      stroke-miterlimit="10"
+    />
+    <path
+      d="M7.75 6.25L10.25 8.5L7.75 10.75"
+      stroke="white"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,9 +3,45 @@ import Header from "~/components/Header.vue";
 </script>
 
 <template>
+  <el-header>
+    <Header />
+  </el-header>
   <el-container class="flex size-full flex-col gap-4 bg-bg_main p-4">
-    <el-header>
-      <Header />
-    </el-header>
+    <div class="flex flex-col">
+      <div class="flex justify-between my-[50px]">
+        <div class="w-[50%]">
+          <h1 class="px-6 py-4 text-[32px]/[38.4px] text-brand_blue">
+            КРАСНАЯ КНИГА
+          </h1>
+          <p class="px-6 text-[18px]/[29px] text-brand_blue">
+            Безусловно, внедрение современных методик обеспечивает широкому
+            кругу (специалистов) участие в формировании первоочередных
+            требований. Банальные, но неопровержимые выводы, а также действия
+            представителей оппозиции преданы социально-демократической анафеме.
+            Для современного мира дальнейшее развитие различных форм
+            деятельности предопределяет высокую востребованность новых
+            предложений.
+          </p>
+        </div>
+        <div class="w-full max-w-[50%]">
+          <img
+            src="../assets/animal.jpg"
+            alt=""
+            class="w-full rounded-[60px]"
+          />
+        </div>
+      </div>
+      <div>
+        <h2 class="px-6 py-4 text-[32px]/[38.4px] text-brand_blue">
+          Интерактивная карта краснокнижных животных
+        </h2>
+      </div>
+    </div>
   </el-container>
 </template>
+
+<style>
+.el-header {
+  padding: 0;
+}
+</style>
