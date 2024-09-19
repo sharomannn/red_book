b9e14bd0e9a0dc83992dfc6cf4c6287369ec49e9
andreyBelyaev111 <uvnrejnvekjrnvuvjehvjoevjow@gmail.com>
2024-09-19 08:05:40 +0300

    вёрстка #26

---
+++ frontend/components/DrawerNavigationMenu.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+const { show } = defineProps<{
+  show: boolean;
+}>();
+
+const emits = defineEmits<{
+  (e: "update:modelValue", flag: boolean): void;
+}>();
+</script>
+
+<template>
+  <ElDrawer
+    title="КРАСНАЯ КНИГА"
+    size="14%"
+    direction="ltr"
+    :with-header="false"
+    :model-value="show"
+    @close="emits('update:modelValue', false)"
+  >
+    <div>
+      <h2
+        class="m-auto mb-4 px-6 py-4 text-[32px]/[38.4px] font-bold text-brand_blue"
+      >
+        КРАСНАЯ КНИГА
+      </h2>
+      <ElMenu>
+        <ElMenuItem class="text-[14px]/[22.4px] text-text_main">
+          <ElIcon class="text-inherit">
+            <TableCellIcon />
+          </ElIcon>
+          <span> Активные обращения </span>
+        </ElMenuItem>
+
+        <ElMenuItem class="text-[14px]/[22.4px] text-text_main">
+          <ElIcon class="text-inherit">
+            <DocumentEditingIcon />
+          </ElIcon>
+          <span> Архив обращений </span>
+        </ElMenuItem>
+
+        <ElMenuItem class="text-[14px]/[22.4px] text-text_main">
+          <ElIcon>
+            <Phone />
+          </ElIcon>
+          <span> Журнал звонков </span>
+        </ElMenuItem>
+
+        <!-- <ElMenuItem
+          :index="CALL_CENTER_ROUTE_NAMES.KNOWLEDGE_BASE"
+          class="text-[14px]/[22.4px] text-text_main"
+        >
+          <ElIcon>
+            <InfoOutlineIcon />
+          </ElIcon>
+          <span> База знаний </span>
+        </ElMenuItem> -->
+
+        <ElMenuItem class="text-[14px]/[22.4px] text-text_main">
+          <ElIcon>
+            <DataAnalysis />
+          </ElIcon>
+          <span> Статистика </span>
+        </ElMenuItem>
+      </ElMenu>
+    </div>
+  </ElDrawer>
+</template>
+
+<style scoped lang="scss">
+.el-menu {
+  border-right: 0;
+}
+
+.el-sub-menu__title {
+  padding-right: 0;
+}
+
+:deep(.el-menu .el-sub-menu__title .el-sub-menu__icon-arrow) {
+  position: initial;
+  margin-top: 2px;
+  margin-left: 8px;
+
+  svg {
+    width: 16px;
+    height: 16px;
+  }
+}
+
+:deep(.el-menu .el-menu-item [class^="el-icon"]) {
+  margin-right: 8px;
+}
+
+:deep(.el-menu .el-menu-item-group__title) {
+  display: none;
+}
+</style>
---
+++ frontend/components/Header.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import DrawerNavigationMenu from "./DrawerNavigationMenu.vue";
+
+const showNavigationDrawer = ref(false);
+// const item = {
+//   date: "2016-05-02",
+//   name: "Tom",
+//   address: "No. 189, Grove St, Los Angeles",
+// };
+// const tableData = ref(Array.from({ length: 20 }).fill(item));
+</script>
+
+<template>
+  <div>
+    <div class="flex h-[4vh] w-full items-center justify-between">
+      <div class="flex items-center">
+        <ElIcon
+          size="32"
+          class="my-[3px] mr-4 cursor-pointer"
+          @click="showNavigationDrawer = true"
+        >
+        </ElIcon>
+
+        <ElLink :underline="false" @click="">
+          <h1 class="mr-6 text-[32px]/[38.4px] font-bold text-brand_blue">
+            ЦОВ 112
+          </h1>
+        </ElLink>
+      </div>
+
+      <div class="flex items-center justify-center gap-2">
+        <ElButton ref="buttonRef" size="small" round> </ElButton>
+      </div>
+
+      <Teleport to="body">
+        <DrawerNavigationMenu v-model:show="showNavigationDrawer" />
+      </Teleport>
+    </div>
+  </div>
+</template>
